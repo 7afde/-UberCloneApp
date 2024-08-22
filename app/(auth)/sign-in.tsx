0c5a21db1a0b019c275facc24545b2a02ac8e000
hafed <hafed.zouyed@univ-constantine2.dz>
2024-08-22 18:08:55 +0100

    ading Sign in (UI)

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,67 @@
-import { View, Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+// eslint-disable-next-line import/no-unresolved
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { View, Image, Text, ScrollView } from "react-native";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const onSignInPress = async () => {
+    console.log("Sign In Pressed");
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative h-[250px] w-full">
+          <Image source={images.signUpCar} className="z-0 h-[250px] w-full" />
+          <Text className="absolute bottom-5 left-5 font-JakartaSemiBold text-2xl text-black">
+            Welcome Back 👋🏼
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChange={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            onChange={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Sign In"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/(auth)/sign-up"
+            className="mt-10 text-center text-lg text-general-200"
+          >
+            <Text>Don't have an account? </Text>
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
+
+        {/* Verification model */}
+      </View>
+    </ScrollView>
   );
 };
 
